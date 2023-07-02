92a726c67baf35a1f137e9e8043d0db21e084fa6
Maksull <kyrkyrkyr99@gmail.com>
2023-07-02 21:32:51 +0300

    Add Maperly.

---
+++ Infrastructure/Mapperly/UserMapper.cs
@@ -0,0 +1,13 @@
+﻿using Core.Contracts.Controllers.Auth;
+using Core.Entities;
+using Riok.Mapperly.Abstractions;
+
+namespace Infrastructure.Mapperly
+{
+    [Mapper]
+    public static partial class UserMapper
+    {
+        [MapProperty(nameof(RegisterRequest.Username), nameof(User.UserName))]
+        public static partial User RegisterRequestToUser(RegisterRequest registerRequest);
+    }
+}
