2970fe0688b13f6e320bb750dc4b9407b8040991
Maksull <kyrkyrkyr99@gmail.com>
2023-07-03 18:30:06 +0300

    Configure Dependencies.

---
+++ Dependencies/Dependencies.cs
@@ -0,0 +1,136 @@
+﻿using Core.Entities;
+using Core.Validators.Auth;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Infrastructure.Data;
+using Infrastructure.Mediator.Auth;
+using Infrastructure.Services;
+using Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Dependencies
+{
+    public static class Dependencies
+    {
+        public static IServiceCollection ConfigureDI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.ConfigureDbContext(configuration);
+            services.ConfigureJwt(configuration);
+            services.ConfigureServices();
+            services.ConfigureMediatR();
+            services.ConfigureFluentValidation();
+
+            return services;
+        }
+
+        public static WebApplication MigrateDb(this WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                using (var appContext = scope.ServiceProvider.GetRequiredService<ApiDataContext>())
+                {
+                    try
+                    {
+                        appContext.Database.Migrate();
+
+                        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+
+                        var user = userManager.FindByNameAsync("Admin").Result!;
+
+                        var result = userManager.AddToRoleAsync(user, "Admin").Result;
+
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(ex.Message);
+                    }
+                }
+            }
+
+            return app;
+        }
+
+        private static IServiceCollection ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApiDataContext>(opts =>
+            {
+                opts.UseNpgsql(configuration["ConnectionStrings:SignalRChat"]);
+            });
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApiDataContext>()
+                .AddDefaultTokenProviders();
+
+            return services;
+        }
+
+        private static IServiceCollection ConfigureJwt(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(opts =>
+            {
+                opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(opts =>
+            {
+                opts.TokenValidationParameters = new()
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:SecurityKey"]!)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.FromSeconds(5),
+                };
+                opts.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+
+                        // If the request is for our hub...
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/chathub")))
+                        {
+                            // Read the token out of the query string
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+            return services;
+        }
+
+        private static IServiceCollection ConfigureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IEmailService, EmailService>();
+
+            return services;
+        }
+
+        private static IServiceCollection ConfigureMediatR(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<LoginHandler>());
+
+            return services;
+        }
+
+        private static IServiceCollection ConfigureFluentValidation(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(typeof(LoginRequestValidator).Assembly);
+
+            services.AddFluentValidationAutoValidation();
+
+            return services;
+        }
+    }
+}
